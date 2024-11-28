ad91259a65c650cc73e3e4063d4591694bbec79c
RichardGbengaEhinder <rehindero@africaexchange.com>
2024-11-28 17:07:24 +0100

    fix error

--- build.sh
+++ build.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
+pip install --no-deps twisted
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-if [[ $CREATE_SUPERUSER]];
-then
-   python manage.py createsuperuser --no-input --email "$DJANGO_SUPER_USER_EMAIL"
-fi
